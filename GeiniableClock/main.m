49861da9be31de9ea73a908df4ad2d322d37ea39
kibernet <kibernet@sina.com>
2015-06-15 10:40:34 +0800

    init

---
+++ GeiniableClock/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GeiniableClock
+//
+//  Created by yuan jun on 11-8-5.
+//  Copyright 2011 the9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
